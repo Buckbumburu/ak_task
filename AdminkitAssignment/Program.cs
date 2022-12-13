784ca6d985efc585bf3e080c90e8a3a461b42385
Kristian <do-not-reply.test.test@test.test>
2022-12-13 19:22:55 +0100

    Simple redirection logic to force the page to stay at index.html

--- AdminkitAssignment/Program.cs
+++ AdminkitAssignment/Program.cs
@@ -1,13 +1,28 @@
 using Microsoft.Extensions.FileProviders;
 
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+WebApplication app = builder.Build();
 
+//Host files of the built angular application
 app.UseStaticFiles(new StaticFileOptions
 {
     FileProvider = new PhysicalFileProvider(
         Path.Combine(builder.Environment.ContentRootPath, "angular", "dist", "angular")),
     RequestPath = new PathString(string.Empty)
+});
+
+//Simple redirect to index.html of all requests except those for controllers
+app.Use(async (context, next) =>
+{
+    string requestPath = context.Request.Path.ToString();
+
+    if (!requestPath.StartsWith("/api"))
+    {
+        context.Response.Redirect("/index.html");
+        return;
+    }
+
+    await next(context);
 });
 
 app.Run();
